3cabe22e3ba6a79d9e065e7173fbfb3e644250cb
Nafisa Parveen <103953608+Nafisa1117@users.noreply.github.com>
2023-09-25 22:52:24 +0530

    Stack pratise 1

---
+++ Core Java All/stackPractise1/Main.java
@@ -0,0 +1,22 @@
+package stackPractise1;
+
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack<String> stack = new Stack<>();
+		
+		stack.push("Nafisa");
+		stack.push("Namgo");
+		stack.push("flamingo");
+		stack.push("green");
+		
+		System.out.println(stack);
+		
+
+	}
+
+}
